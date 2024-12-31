6d5738fbad47b4e6f45e6fafe653c634e148502c
Arham1243 <arham404khan@gmail.com>
2024-12-31 21:26:46 +0500

    refactor assets

--- resources/views/frontend/layouts/scripts.blade.php
+++ resources/views/frontend/layouts/scripts.blade.php
@@ -1,7 +1,7 @@
-<script src="{{ asset('assets/js/all.min.js') }}"></script>
-<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/all.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/app.js') }}"></script>
 
-<script src="{{ asset('admin/login/assets/js/jquery.toast.js') }}"></script>
+<script src="{{ asset('toast/js/jquery.toast.js') }}"></script>
 
 {{-- @if (Auth::guard('admin'))
     <script src="{{ asset('admin/assets/js/ckeditor/ckeditor.js') }}" type="text/javascript"></script>
