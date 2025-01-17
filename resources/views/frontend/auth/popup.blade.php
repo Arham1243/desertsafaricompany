f3b9bf2ab5d148883316f5aac14479fdbaaae107
Arham1243 <arham404khan@gmail.com>
2025-01-18 01:39:09 +0500

    fix mounting

--- resources/views/frontend/auth/popup.blade.php
+++ resources/views/frontend/auth/popup.blade.php
@@ -1,13 +1,13 @@
-<div id="app">
-    @if (!Auth::check())
+@if (!Auth::check())
+    <div id="app">
         @include('frontend.auth.src.popup')
-    @else
-        <a href="{{ route('auth.logout') }}" onclick="return confirm('Are you sure you want to Logout?')" title="Logout"
-            class="item__become-supplier">
-            <span><b>Logout</b></span>
-        </a>
-    @endif
-</div>
+    </div>
+@else
+    <a href="{{ route('auth.logout') }}" onclick="return confirm('Are you sure you want to Logout?')" title="Logout"
+        class="item__become-supplier">
+        <span><b>Logout</b></span>
+    </a>
+@endif
 <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 @if (!Auth::check())
