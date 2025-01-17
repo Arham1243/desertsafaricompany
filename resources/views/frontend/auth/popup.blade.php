b2860b2c82f23fe7f1d5298c8df217d09a5f72e2
Arham1243 <arham404khan@gmail.com>
2025-01-18 01:41:10 +0500

    fix vue js mouting

--- resources/views/frontend/auth/popup.blade.php
+++ resources/views/frontend/auth/popup.blade.php
@@ -1,17 +1,15 @@
-@if (!Auth::check())
-    <div id="app">
+<div id="login-popup">
+    @if (!Auth::check())
         @include('frontend.auth.src.popup')
-    </div>
-@else
-    <a href="{{ route('auth.logout') }}" onclick="return confirm('Are you sure you want to Logout?')" title="Logout"
-        class="item__become-supplier">
-        <span><b>Logout</b></span>
-    </a>
-@endif
+    @else
+        <a href="{{ route('auth.logout') }}" onclick="return confirm('Are you sure you want to Logout?')" title="Logout"
+            class="item__become-supplier">
+            <span><b>Logout</b></span>
+        </a>
+    @endif
+</div>
 <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-@if (!Auth::check())
-    @section('js')
-        @include('frontend.auth.src.js.popup')
-    @endsection
-@endif
+@section('js')
+    @include('frontend.auth.src.js.popup')
+@endsection
--- resources/views/frontend/auth/src/js/popup.blade.php
+++ resources/views/frontend/auth/src/js/popup.blade.php
@@ -5,186 +5,188 @@
         computed,
         watch
     } = Vue;
+    @if (!Auth::check())
+        const PopupComponent = {
+            setup() {
+                const loginPopup = ref(null);
+                const showPassword = ref(null);
+                const email = ref('');
+                const formData = ref({
+                    full_name: '',
+                    password: '',
+                    remember: false,
+                });
+                const loading = ref(false);
+                const challenge = ref('check_email');
 
-    const PopupComponent = {
-        setup() {
-            const loginPopup = ref(null);
-            const showPassword = ref(null);
-            const email = ref('');
-            const formData = ref({
-                full_name: '',
-                password: '',
-                remember: false,
-            });
-            const loading = ref(false);
-            const challenge = ref('check_email');
-
-            const togglePasswordVisibility = () => {
-                showPassword.value = !showPassword.value;
-            }
-
-            const openLoginPopup = () => {
-                loginPopup.value.classList.add('open');
-            };
-
-            const closeLoginPopup = () => {
-                loginPopup.value.classList.remove('open');
-
-                setTimeout(() => {
-                    resetForm()
-                }, 400);
-            };
-
-            const resetForm = () => {
-                email.value = ''
-                formData.value.full_name = ''
-                formData.value.password = ''
-                formData.value.remember = false
-                challenge.value = 'check_email'
-            }
-
-            const isEmailValid = computed(() => {
-                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                return emailRegex.test(email.value);
-            });
-
-            const isSignUpValid = computed(() => {
-                return formData.value.full_name.trim() !== '' && formData.value.password.trim() !== '' &&
-                    formData.value.password.length > 7;
-            });
-
-            const isLoginValid = computed(() => {
-                return isEmailValid && formData.value.password.trim() !== '' &&
-                    formData.value.password.length > 7;
-            });
-
-            const showToast = (type, message) => {
-                if (type === 'error') {
-                    $.toast({
-                        heading: 'Error!',
-                        position: 'bottom-right',
-                        loaderBg: '#ff6849',
-                        icon: 'error',
-                        hideAfter: 5000,
-                        text: message,
-                        stack: 6
-                    });
-                } else {
-                    $.toast({
-                        text: message,
-                        heading: 'Success!',
-                        position: 'bottom-right',
-                        loaderBg: '#ff6849',
-                        icon: 'success',
-                        hideAfter: 2000,
-                        stack: 6
-                    });
+                const togglePasswordVisibility = () => {
+                    showPassword.value = !showPassword.value;
                 }
-            }
 
-            const checkEmail = async () => {
-                try {
-                    loading.value = true;
-                    let route = `{{ route('auth.check-email') }}?email=${email.value}`
-                    const response = await axios.post(route)
-                    if (response.data.challenge == 'sign_up') {
-                        challenge.value = 'sign_up';
-                    } else if (response.data.challenge == 'login') {
-                        challenge.value = 'login';
-                    } else if (response.data.challenge == 'forgot_password') {
-                        challenge.value = 'forgot_password';
+                const openLoginPopup = () => {
+                    loginPopup.value.classList.add('open');
+                };
+
+                const closeLoginPopup = () => {
+                    loginPopup.value.classList.remove('open');
+
+                    setTimeout(() => {
+                        resetForm()
+                    }, 400);
+                };
+
+                const resetForm = () => {
+                    email.value = ''
+                    formData.value.full_name = ''
+                    formData.value.password = ''
+                    formData.value.remember = false
+                    challenge.value = 'check_email'
+                }
+
+                const isEmailValid = computed(() => {
+                    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                    return emailRegex.test(email.value);
+                });
+
+                const isSignUpValid = computed(() => {
+                    return formData.value.full_name.trim() !== '' && formData.value.password.trim() !==
+                        '' &&
+                        formData.value.password.length > 7;
+                });
+
+                const isLoginValid = computed(() => {
+                    return isEmailValid && formData.value.password.trim() !== '' &&
+                        formData.value.password.length > 7;
+                });
+
+                const showToast = (type, message) => {
+                    if (type === 'error') {
+                        $.toast({
+                            heading: 'Error!',
+                            position: 'bottom-right',
+                            loaderBg: '#ff6849',
+                            icon: 'error',
+                            hideAfter: 5000,
+                            text: message,
+                            stack: 6
+                        });
+                    } else {
+                        $.toast({
+                            text: message,
+                            heading: 'Success!',
+                            position: 'bottom-right',
+                            loaderBg: '#ff6849',
+                            icon: 'success',
+                            hideAfter: 2000,
+                            stack: 6
+                        });
                     }
-                } catch (error) {
-                    showToast('error', error.response.data.message)
-                } finally {
-                    loading.value = false;
                 }
-            };
 
-            const performAuth = async () => {
-                try {
-                    loading.value = true;
-                    let route = `{{ route('auth.perform-auth') }}`;
-
-                    const payload = {
-                        auth_type: challenge.value === 'login' ? 'login' : 'sign_up',
-                        email: email.value,
-                        ...formData.value,
-                    };
-
-                    const response = await axios.post(route, payload);
-
-                    if (response.data.status === 'success') {
-                        showToast('success', response.data.message);
-                        if (response.data.redirect_url) {
-                            window.location.href = response.data.redirect_url;
+                const checkEmail = async () => {
+                    try {
+                        loading.value = true;
+                        let route = `{{ route('auth.check-email') }}?email=${email.value}`
+                        const response = await axios.post(route)
+                        if (response.data.challenge == 'sign_up') {
+                            challenge.value = 'sign_up';
+                        } else if (response.data.challenge == 'login') {
+                            challenge.value = 'login';
+                        } else if (response.data.challenge == 'forgot_password') {
+                            challenge.value = 'forgot_password';
                         }
-                    } else {
-                        showToast('error', response.data.message);
+                    } catch (error) {
+                        showToast('error', error.response.data.message)
+                    } finally {
+                        loading.value = false;
                     }
-                } catch (error) {
-                    if (error.response && error.response.data.message) {
-                        showToast('error', error.response.data.message);
-                    } else {
-                        showToast('error', 'Something went wrong. Please try again.');
-                    }
-                } finally {
-                    loading.value = false;
-                }
-            };
+                };
 
-            const resetPasswordRequest = async () => {
-                try {
-                    loading.value = true;
-                    let route = `{{ route('password.email') }}`;
+                const performAuth = async () => {
+                    try {
+                        loading.value = true;
+                        let route = `{{ route('auth.perform-auth') }}`;
 
-                    const payload = {
-                        email: email.value,
-                    };
+                        const payload = {
+                            auth_type: challenge.value === 'login' ? 'login' : 'sign_up',
+                            email: email.value,
+                            ...formData.value,
+                        };
 
-                    const response = await axios.post(route, payload);
+                        const response = await axios.post(route, payload);
 
-                    if (response.data.status === 'success') {
-                        showToast('success', response.data.message);
-                        if (response.data.redirect_url) {
-                            window.location.href = response.data.redirect_url;
+                        if (response.data.status === 'success') {
+                            showToast('success', response.data.message);
+                            if (response.data.redirect_url) {
+                                window.location.href = response.data.redirect_url;
+                            }
+                        } else {
+                            showToast('error', response.data.message);
                         }
-                    } else {
-                        showToast('error', response.data.message);
+                    } catch (error) {
+                        if (error.response && error.response.data.message) {
+                            showToast('error', error.response.data.message);
+                        } else {
+                            showToast('error', 'Something went wrong. Please try again.');
+                        }
+                    } finally {
+                        loading.value = false;
                     }
-                } catch (error) {
-                    if (error.response && error.response.data.message) {
-                        showToast('error', error.response.data.message);
-                    } else {
-                        showToast('error', 'Something went wrong. Please try again.');
+                };
+
+                const resetPasswordRequest = async () => {
+                    try {
+                        loading.value = true;
+                        let route = `{{ route('password.email') }}`;
+
+                        const payload = {
+                            email: email.value,
+                        };
+
+                        const response = await axios.post(route, payload);
+
+                        if (response.data.status === 'success') {
+                            showToast('success', response.data.message);
+                            if (response.data.redirect_url) {
+                                window.location.href = response.data.redirect_url;
+                            }
+                        } else {
+                            showToast('error', response.data.message);
+                        }
+                    } catch (error) {
+                        if (error.response && error.response.data.message) {
+                            showToast('error', error.response.data.message);
+                        } else {
+                            showToast('error', 'Something went wrong. Please try again.');
+                        }
+                    } finally {
+                        loading.value = false;
                     }
-                } finally {
-                    loading.value = false;
-                }
-            };
+                };
 
 
-            return {
-                loginPopup,
-                email,
-                formData,
-                loading,
-                challenge,
-                showPassword,
+                return {
+                    loginPopup,
+                    email,
+                    formData,
+                    loading,
+                    challenge,
+                    showPassword,
 
-                isEmailValid,
-                isSignUpValid,
-                isLoginValid,
+                    isEmailValid,
+                    isSignUpValid,
+                    isLoginValid,
 
-                openLoginPopup,
-                closeLoginPopup,
-                checkEmail,
-                performAuth,
-                resetPasswordRequest,
-                showToast,
-                togglePasswordVisibility
-            };
-        }
-    };
-    Vue.createApp(PopupComponent).mount('#app');
+                    openLoginPopup,
+                    closeLoginPopup,
+                    checkEmail,
+                    performAuth,
+                    resetPasswordRequest,
+                    showToast,
+                    togglePasswordVisibility
+                };
+            }
+        };
+    @endif
+    Vue.createApp(PopupComponent).mount('#login-popup');
 </script>
