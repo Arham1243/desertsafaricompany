c8637911c2d4559931cfbb83152a50d96c5d9a3b
Arham1243 <arham404khan@gmail.com>
2025-01-24 20:41:53 +0500

    add total payments in dashboard home

--- app/Http/Controllers/Admin/AdminDashController.php
+++ app/Http/Controllers/Admin/AdminDashController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\ImageTable;
+use App\Models\Order;
 use App\Models\Tour;
 use App\Models\User;
 use Schema;
@@ -20,7 +21,8 @@ class AdminDashController extends Controller
     {
         $users = User::where('is_active', 1)->get();
         $tours = Tour::where('status', 'publish')->get();
-        $data = compact('users', 'tours');
+        $totalPayments = Order::where('payment_status', 'paid')->sum('total_amount');
+        $data = compact('users', 'tours', 'totalPayments');
 
         return view('admin.dashboard')->with('title', 'Dashboard')->with($data);
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -27,7 +27,7 @@
                             <div class="revenue-card__icon"><i class='bx bx-dollar'></i></div>
                             <div class="revenue-card__content">
                                 <div class="title">Monthly Online Payments</div>
-                                <div class="num">{{ env('APP_CURRENCY') }}100000</div>
+                                <div class="num">{{ formatPrice($totalPayments) }}</div>
                             </div>
                         </div>
                     </div>
