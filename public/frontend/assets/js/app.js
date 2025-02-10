8c983cbd8941cb5eca5e04e0fccf6080e735e28d
Arham1243 <arham404khan@gmail.com>
2025-02-11 04:35:04 +0500

    fix empty editor para

--- public/frontend/assets/js/app.js
+++ public/frontend/assets/js/app.js
@@ -160,6 +160,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const todayDate = new Date().toISOString().split("T")[0];
+document.getElementById("start_date")?.setAttribute("min", todayDate);
+const emptyParas = Array.from(document.querySelectorAll("p")).filter(
+    (p) => p.innerHTML.trim() === "&nbsp;"
+);
+
+if (emptyParas.length > 0) {
+    emptyParas.forEach((p) => (p.classList.add('empty')));
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("[data-show-more]").forEach((container) => {
