e1d95d314becbae8b6f5cd8aba23e022ba8de126
Arham1243 <arham404khan@gmail.com>
2025-02-11 04:43:49 +0500

    fix table name

--- database/migrations/2025_02_11_043715_add_inclusions_exclusions_to_table_name.php
+++ database/migrations/2025_02_11_043715_add_inclusions_exclusions_to_table_name.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('table_name', function (Blueprint $table) {
+        Schema::table('tours', function (Blueprint $table) {
             $table->json('exclusions_inclusions_heading')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('table_name', function (Blueprint $table) {
+        Schema::table('tours', function (Blueprint $table) {
             $table->dropColumn(['exclusions_inclusions_heading']);
         });
     }
