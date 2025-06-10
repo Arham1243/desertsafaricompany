e7a389a12f83ce596d597bcfd26430da131de80d
Arham1243 <arham404khan@gmail.com>
2025-06-11 04:38:10 +0500

    fix: switch condition

--- resources/views/admin/pages/pages-management/edit.blade.php
+++ resources/views/admin/pages/pages-management/edit.blade.php
@@ -38,8 +38,8 @@
                                     <div class="form-check form-switch" data-enabled-text="Show Page Content"
                                         data-disabled-text="Show Page Content">
                                         <input class="form-check-input" data-toggle-switch=""
-                                            {{ $page->show_page_builder_sections === 0 ? 'checked' : '' }} type="checkbox"
-                                            id="show_page_builder_sections" value="1"
+                                            {{ $page->show_page_builder_sections === 1 ? 'checked' : '' }} type="checkbox"
+                                            id="show_page_builder_sections" value="0"
                                             name="show_page_builder_sections">
                                         <label class="form-check-label" for="show_page_builder_sections">Show Page
                                             Content</label>
