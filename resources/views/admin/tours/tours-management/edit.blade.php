e30a02d554d87366e6d04baa92558ce3cc72d0db
Arham1243 <arham404khan@gmail.com>
2025-08-22 01:08:51 +0500

    fix availability_open_hours

--- resources/views/admin/tours/tours-management/edit.blade.php
+++ resources/views/admin/tours/tours-management/edit.blade.php
@@ -2758,7 +2758,12 @@
                                                                         '23:00:00',
                                                                     ];
 
-                                                                    $openHour = $tour->availability_open_hours ?? [];
+                                                                    $openHour = $tour->availability_open_hours
+                                                                        ? json_decode(
+                                                                            $tour->availability_open_hours,
+                                                                            true,
+                                                                        )
+                                                                        : [];
 
                                                                     $openHour = array_replace(
                                                                         array_fill(0, count($days), [
