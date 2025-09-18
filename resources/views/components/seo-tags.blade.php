e791b19d4c42311dd5b364ff2dd736639cc29a50
Arham1243 <arham404khan@gmail.com>
2025-09-18 21:22:24 +0500

    add nullify conditions for seo

--- resources/views/components/seo-tags.blade.php
+++ resources/views/components/seo-tags.blade.php
@@ -1,32 +1,32 @@
-<title>{{ $seo->seo_title ?? (isset($title) ? $title . ' | ' . env('APP_NAME') : env('APP_NAME')) }}</title>
+<title>{{ optional($seo)->seo_title ?? (isset($title) ? $title . ' | ' . env('APP_NAME') : env('APP_NAME')) }}</title>
 @if ($seo)
-    @if ($seo->seo_description)
-        <meta name="description" content="{{ $seo->seo_description }}">
+    @if (optional($seo)->seo_description)
+        <meta name="description" content="{{ optional($seo)->seo_description }}">
     @endif
-    <meta name="robots" content="{{ $seo->is_seo_index ? 'index, follow' : 'noindex, nofollow' }}">
-    @if ($seo->canonical)
-        <link rel="canonical" href="{{ $seo->canonical }}">
+    <meta name="robots" content="{{ optional($seo)->is_seo_index ? 'index, follow' : 'noindex, nofollow' }}">
+    @if (optional($seo)->canonical)
+        <link rel="canonical" href="{{ optional($seo)->canonical }}">
     @endif
-    @if ($seo->fb_title)
-        <meta property="og:title" content="{{ $seo->fb_title }}">
+    @if (optional($seo)->fb_title)
+        <meta property="og:title" content="{{ optional($seo)->fb_title }}">
     @endif
-    @if ($seo->fb_description)
-        <meta property="og:description" content="{{ $seo->fb_description }}">
+    @if (optional($seo)->fb_description)
+        <meta property="og:description" content="{{ optional($seo)->fb_description }}">
     @endif
-    @if ($seo->fb_featured_image)
-        <meta property="og:image" content="{{ asset($seo->fb_featured_image) }}">
+    @if (optional($seo)->fb_featured_image)
+        <meta property="og:image" content="{{ asset(optional($seo)->fb_featured_image) }}">
     @endif
-    @if ($seo->tw_title)
+    @if (optional($seo)->tw_title)
         <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="{{ $seo->tw_title }}">
+        <meta name="twitter:title" content="{{ optional($seo)->tw_title }}">
     @endif
-    @if ($seo->tw_description)
-        <meta name="twitter:description" content="{{ $seo->tw_description }}">
+    @if (optional($seo)->tw_description)
+        <meta name="twitter:description" content="{{ optional($seo)->tw_description }}">
     @endif
-    @if ($seo->tw_featured_image)
-        <meta name="twitter:image" content="{{ asset($seo->tw_featured_image) }}">
+    @if (optional($seo)->tw_featured_image)
+        <meta name="twitter:image" content="{{ asset(optional($seo)->tw_featured_image) }}">
     @endif
-    @if ($seo->schema)
-        {!! $seo->schema !!}
+    @if (optional($seo)->schema)
+        {!! optional($seo)->schema !!}
     @endif
 @endif
