bf82de07c5f4db44b7f563c18474604c7848468d
Arham1243 <arham404khan@gmail.com>
2025-11-08 23:05:22 +0500

    refactor: remove timestamp query parameters from asset URLs

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -1,8 +1,8 @@
-<script src="{{ asset('admin/assets/js/jquery.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/dataTables.min.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/select2.min.js?v=' . time()) }}"></script>
-<script src="{{ asset('toast/js/jquery.toast.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/fancybox.min.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/ckeditor.js?v=' . time()) }}"></script>
-<script src="{{ asset('admin/assets/js/app.js?v=' . time()) }}"></script>
+<script src="{{ asset('admin/assets/js/jquery.js') }}"></script>
+<script src="{{ asset('admin/assets/js/dataTables.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/select2.min.js') }}"></script>
+<script src="{{ asset('toast/js/jquery.toast.js') }}"></script>
+<script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/fancybox.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/ckeditor.js') }}"></script>
+<script src="{{ asset('admin/assets/js/app.js') }}"></script>
--- resources/views/frontend/layouts/links.blade.php
+++ resources/views/frontend/layouts/links.blade.php
@@ -5,12 +5,12 @@
 @if ($favicon)
     <link rel="shortcut icon" href="{{ asset($favicon) }}" type="image/x-icon">
 @endif
-<link href="{{ asset('frontend/assets/css/all.min.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('frontend/assets/css/all.min.css') }}" rel="stylesheet">
 <link rel="preload" href="{{ asset('frontend/assets/fonts/boxicons/boxicons.woff2') }}" as="font" type="font/woff2"
     crossorigin="anonymous">
-<link href="{{ asset('frontend/assets/css/style.css?v=' . time()) }}" rel="stylesheet">
-<link href="{{ asset('frontend/assets/css/responsive.css?v=' . time()) }}" rel="stylesheet">
-<link href="{{ asset('toast/css/jquery.toast.css?v=' . time()) }}" rel="stylesheet">
+<link href="{{ asset('frontend/assets/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('frontend/assets/css/responsive.css') }}" rel="stylesheet">
+<link href="{{ asset('toast/css/jquery.toast.css') }}" rel="stylesheet">
 @if ($globalStyles)
     <style>
         {!! $globalStyles !!}
