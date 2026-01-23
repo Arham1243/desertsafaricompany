87b88e06a02e40810b8daf99b5ce5ddeca142326
Arham1243 <arham404khan@gmail.com>
2026-01-24 00:06:45 +0500

    show dial codes correctly for every country

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -14,7 +14,7 @@
                         @if ($settings->get('cookie_bar_accept_bg_color') || $settings->get('cookie_bar_accept_text_color')) style="
                         @if ($settings->get('cookie_bar_accept_bg_color')) background-color: {{ $settings->get('cookie_bar_accept_bg_color') }}; @endif
                         @if ($settings->get('cookie_bar_accept_text_color')) color: {{ $settings->get('cookie_bar_accept_text_color') }}; @endif "
-                                                               @endif>
+                                                                      @endif>
                         {{ $settings->get('cookie_bar_accept_text') ?? 'Accept All' }}
                     </button>
                     <button type="button" class="cookie-consent__button cookie-consent__button--reject"
@@ -22,7 +22,7 @@
                         @if ($settings->get('cookie_bar_reject_bg_color') || $settings->get('cookie_bar_reject_text_color')) style="
                         @if ($settings->get('cookie_bar_reject_bg_color')) background-color: {{ $settings->get('cookie_bar_reject_bg_color') }}; @endif
                         @if ($settings->get('cookie_bar_reject_text_color')) color: {{ $settings->get('cookie_bar_reject_text_color') }}; @endif "
-                                                               @endif>
+                                                                      @endif>
                         {{ $settings->get('cookie_bar_reject_text') ?? 'Reject' }}
                     </button>
                 </div>
@@ -183,17 +183,17 @@
                 : null;
         @endphp
         @if ($is_enabled_login_button)
-        @if (!Auth::check())
-            <a href="javascript:void(0)" open-vue-login-popup class="primary-btn w-75 mx-auto mt-4"
-                @if ($login_button_text_color || $login_button_bg_color) style="{{ $login_button_text_color ? "color: {$login_button_text_color};" : '' }} {{ $login_button_bg_color ? "background-color: {$login_button_bg_color};" : '' }}" @endif>
-                <span><b>{{ $login_button_text }}</b></span>
-            </a>
+            @if (!Auth::check())
+                <a href="javascript:void(0)" open-vue-login-popup class="primary-btn w-75 mx-auto mt-4"
+                    @if ($login_button_text_color || $login_button_bg_color) style="{{ $login_button_text_color ? "color: {$login_button_text_color};" : '' }} {{ $login_button_bg_color ? "background-color: {$login_button_bg_color};" : '' }}" @endif>
+                    <span><b>{{ $login_button_text }}</b></span>
+                </a>
             @else
-            <a href="{{ route('user.dashboard') }}" class="primary-btn w-75 mx-auto mt-4"
-                @if ($login_button_text_color || $login_button_bg_color) style="{{ $login_button_text_color ? "color: {$login_button_text_color};" : '' }} {{ $login_button_bg_color ? "background-color: {$login_button_bg_color};" : '' }}" @endif>
-                <span><b>Dashboard</b></span>
-            </a>
-               @endif
+                <a href="{{ route('user.dashboard') }}" class="primary-btn w-75 mx-auto mt-4"
+                    @if ($login_button_text_color || $login_button_bg_color) style="{{ $login_button_text_color ? "color: {$login_button_text_color};" : '' }} {{ $login_button_bg_color ? "background-color: {$login_button_bg_color};" : '' }}" @endif>
+                    <span><b>Dashboard</b></span>
+                </a>
+            @endif
         @endif
     @endif
 </div>
@@ -411,32 +411,8 @@
                         <div class="row g-0">
                             <div class="col-md-6">
                                 <div class="field">
-                                    <label class="title">Phone number</label>
-                                    <input id="phone_number" type="text" name="phone" class="field"
-                                        inputmode="numeric" pattern="[0-9]*"
-                                        value="{{ old('phone', Auth::user()->phone) }}"
-                                        oninput="this.value = this.value.replace(/[^0-9]/g, '');" maxlength="15">
-                                    @error('phone')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="field">
-                                    <label class="title">DOB</label>
-                                    <input onclick="this.showPicker()" onfocus="this.showPicker()" id="dob" type="date" name="dob" class="field"
-                                        value="{{ old('dob', Auth::user()->dob) }}">
-                                    @error('dob')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="field">
                                     <label class="title">Country</label>
-                                    <select id="country" name="country" required>
+                                    <select id="header-country" name="country" required>
                                         <option value="" selected disabled>Select</option>
                                         @foreach ($countries as $country)
                                             <option value="{{ $country }}"
@@ -461,6 +437,30 @@
                                     @enderror
                                 </div>
                             </div>
+
+                            <div class="col-md-6">
+                                <div class="field">
+                                    <label class="title">Phone number</label>
+                                    <input id="header-phone_number" type="text" name="phone" class="field"
+                                        inputmode="numeric" 
+                                        value="{{ old('phone', Auth::user()->phone) }}">
+                                    @error('phone')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="field">
+                                    <label class="title">DOB</label>
+                                    <input onclick="this.showPicker()" id="dob"
+                                        type="date" name="dob" class="field"
+                                        value="{{ old('dob', Auth::user()->dob) }}">
+                                    @error('dob')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
                             <div class="col-md-12">
                                 <div class="field">
                                     <button class="ms-auto primary-btn">Submit</button>
@@ -472,6 +472,217 @@
             </div>
         </div>
     @endif
+    @push('js')
+        <script>
+            const countryDialCodes = {
+                "afghanistan": "+93",
+                "albania": "+355",
+                "algeria": "+213",
+                "american samoa": "+1-684",
+                "andorra": "+376",
+                "angola": "+244",
+                "anguilla": "+1-264",
+                "antigua and barbuda": "+1-268",
+                "argentina": "+54",
+                "armenia": "+374",
+                "aruba": "+297",
+                "australia": "+61",
+                "austria": "+43",
+                "azerbaijan": "+994",
+                "bahamas": "+1-242",
+                "bahrain": "+973",
+                "bangladesh": "+880",
+                "barbados": "+1-246",
+                "belarus": "+375",
+                "belgium": "+32",
+                "belize": "+501",
+                "benin": "+229",
+                "bermuda": "+1-441",
+                "bhutan": "+975",
+                "bolivia": "+591",
+                "bosnia and herzegovina": "+387",
+                "botswana": "+267",
+                "brazil": "+55",
+                "british indian ocean territory": "+246",
+                "brunei": "+673",
+                "bulgaria": "+359",
+                "burkina faso": "+226",
+                "burundi": "+257",
+                "cambodia": "+855",
+                "cameroon": "+237",
+                "canada": "+1",
+                "cape verde": "+238",
+                "cayman islands": "+1-345",
+                "central african republic": "+236",
+                "chad": "+235",
+                "chile": "+56",
+                "china": "+86",
+                "colombia": "+57",
+                "comoros": "+269",
+                "congo": "+242",
+                "costa rica": "+506",
+                "croatia": "+385",
+                "cuba": "+53",
+                "cyprus": "+357",
+                "czech republic": "+420",
+                "denmark": "+45",
+                "djibouti": "+253",
+                "dominica": "+1-767",
+                "dominican republic": "+1-809",
+                "ecuador": "+593",
+                "egypt": "+20",
+                "el salvador": "+503",
+                "equatorial guinea": "+240",
+                "eritrea": "+291",
+                "estonia": "+372",
+                "eswatini": "+268",
+                "ethiopia": "+251",
+                "fiji": "+679",
+                "finland": "+358",
+                "france": "+33",
+                "gabon": "+241",
+                "gambia": "+220",
+                "georgia": "+995",
+                "germany": "+49",
+                "ghana": "+233",
+                "greece": "+30",
+                "grenada": "+1-473",
+                "guatemala": "+502",
+                "guinea": "+224",
+                "guinea bissau": "+245",
+                "guyana": "+592",
+                "haiti": "+509",
+                "honduras": "+504",
+                "hungary": "+36",
+                "iceland": "+354",
+                "india": "+91",
+                "indonesia": "+62",
+                "iran": "+98",
+                "iraq": "+964",
+                "ireland": "+353",
+                "israel": "+972",
+                "italy": "+39",
+                "jamaica": "+1-876",
+                "japan": "+81",
+                "jordan": "+962",
+                "kazakhstan": "+7",
+                "kenya": "+254",
+                "kiribati": "+686",
+                "kuwait": "+965",
+                "kyrgyzstan": "+996",
+                "laos": "+856",
+                "latvia": "+371",
+                "lebanon": "+961",
+                "lesotho": "+266",
+                "liberia": "+231",
+                "libya": "+218",
+                "liechtenstein": "+423",
+                "lithuania": "+370",
+                "luxembourg": "+352",
+                "madagascar": "+261",
+                "malawi": "+265",
+                "malaysia": "+60",
+                "maldives": "+960",
+                "mali": "+223",
+                "malta": "+356",
+                "marshall islands": "+692",
+                "mauritania": "+222",
+                "mauritius": "+230",
+                "mexico": "+52",
+                "micronesia": "+691",
+                "moldova": "+373",
+                "monaco": "+377",
+                "mongolia": "+976",
+                "montenegro": "+382",
+                "morocco": "+212",
+                "mozambique": "+258",
+                "myanmar": "+95",
+                "namibia": "+264",
+                "nauru": "+674",
+                "nepal": "+977",
+                "netherlands": "+31",
+                "new zealand": "+64",
+                "nicaragua": "+505",
+                "niger": "+227",
+                "nigeria": "+234",
+                "north macedonia": "+389",
+                "norway": "+47",
+                "oman": "+968",
+                "pakistan": "+92",
+                "palau": "+680",
+                "palestine": "+970",
+                "panama": "+507",
+                "papua new guinea": "+675",
+                "paraguay": "+595",
+                "peru": "+51",
+                "philippines": "+63",
+                "poland": "+48",
+                "portugal": "+351",
+                "qatar": "+974",
+                "romania": "+40",
+                "russia": "+7",
+                "rwanda": "+250",
+                "saudi arabia": "+966",
+                "senegal": "+221",
+                "serbia": "+381",
+                "seychelles": "+248",
+                "sierra leone": "+232",
+                "singapore": "+65",
+                "slovakia": "+421",
+                "slovenia": "+386",
+                "solomon islands": "+677",
+                "somalia": "+252",
+                "south africa": "+27",
+                "spain": "+34",
+                "sri lanka": "+94",
+                "sudan": "+249",
+                "suriname": "+597",
+                "sweden": "+46",
+                "switzerland": "+41",
+                "syria": "+963",
+                "taiwan": "+886",
+                "tajikistan": "+992",
+                "tanzania": "+255",
+                "thailand": "+66",
+                "timor leste": "+670",
+                "togo": "+228",
+                "tonga": "+676",
+                "trinidad and tobago": "+1-868",
+                "tunisia": "+216",
+                "turkey": "+90",
+                "turkmenistan": "+993",
+                "tuvalu": "+688",
+                "uganda": "+256",
+                "ukraine": "+380",
+                "united arab emirates": "+971",
+                "united kingdom": "+44",
+                "united states": "+1",
+                "uruguay": "+598",
+                "uzbekistan": "+998",
+                "vanuatu": "+678",
+                "venezuela": "+58",
+                "vietnam": "+84",
+                "yemen": "+967",
+                "zambia": "+260",
+                "zimbabwe": "+263"
+            };
+
+            const countrySelect = document.getElementById("header-country");
+            const phoneInput = document.getElementById("header-phone_number");
+
+            countrySelect.addEventListener("change", function() {
+                const country = this.value.toLowerCase();
+                const dialCode = countryDialCodes[country];
+
+                if (!dialCode) return;
+
+                // Only auto-fill if user has not typed yet
+                if (!phoneInput.value || !phoneInput.value.startsWith("+")) {
+                    phoneInput.value = dialCode + " ";
+                }
+            });
+        </script>
+    @endpush
 @endif
 @push('js')
     <script>
